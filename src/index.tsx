19ef2b3a75457517cbec6f01f771246336b6e156
Anton <antondankov93@gmail.com>
2020-06-29 14:28:56 +0300

    added Header component

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+
+
+import {App} from "./node_modules/App";
+import {Header} from "components/header/Header";
+
+import './index.scss'
 
 ReactDOM.render(
-  <React.StrictMode>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
